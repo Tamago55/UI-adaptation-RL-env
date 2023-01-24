46539b8c70aff4290fe0147ee3d9623c6bbed2c9
Daniel Gaspar-Figueiredo <61389510+Dagasfi@users.noreply.github.com>
2023-01-24 13:36:46 +0100

    [UPD]: Actions call in Environment class
    I have defined 7 possible actions.
    0 for NoOperate
    1 for grid layout
    2 for list layout
    3 for light color
    4 for dark color
    5 for default size
    6 for small size
    7 for big size

--- src/uiadaptationenv.py
+++ src/uiadaptationenv.py
@@ -1,6 +1,12 @@
 import gym
 from gym import spaces
 import numpy as np
+from user import User
+from platform_ import Platform
+from environment import Environment 
+from uidesign import UIDesign
+import utils
+
 
 class UIAdaptationEnv (gym.Env):
     '''
@@ -11,13 +17,13 @@ class UIAdaptationEnv (gym.Env):
 
     def __init__(self,render_mode=None):
         self.state = None
-        self.user = None
-        self.platform = None
-        self.environment = None
-        self.uidesign = None
+        self.user = utils.get_random_user()
+        self.platform = utils.get_random_platform()
+        self.environment = utils.get_random_environment()
+        self.uidesign = utils.get_random_ui()
 
         # We have 4 actions, corresponding to "NoOperate", "DefaultLayout", "left", "down", "right"
-        self.action_space = spaces.Discrete(2)
+        self.action_space = spaces.Discrete(8)
 
 
         self.observation_space = gym.spaces.Dict({
@@ -41,6 +47,9 @@ class UIAdaptationEnv (gym.Env):
             })
         })
 
+        
+
+
 
         # assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -50,5 +59,33 @@ class UIAdaptationEnv (gym.Env):
         # return super().render()
 
     def step(self, action):
-        pass
+        if action == 0:
+            # noop
+            pass
+        elif action == 1:
+            # Change to Grid
+            self.uidesign.change_layout('grid')
+        elif action == 2:
+            # Change to List
+            self.uidesign.change_layout('list')
+        elif action == 3:
+            # Change to Light color
+            self.uidesign.change_color_scheme('light')
+        elif action == 4:
+            # Change to Dark color
+            self.uidesign.change_color_scheme('dark')
+        elif action == 5:
+            # Change to default font size.
+            self.uidesign.change_font_size('default')
+        elif action == 6:
+            # Change to small font size
+            self.uidesign.change_font_size('small')
+        elif action == 7:
+            # Change to big font size
+            self.uidesign.change_font_size('big')
+        
+
         #return super().step(action)
+
+   
+

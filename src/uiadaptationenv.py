643e0132935c9d8985e74afb884934e008deb9a9
Daniel Gaspar-Figueiredo <61389510+Dagasfi@users.noreply.github.com>
2023-01-26 13:27:57 +0100

    [UPD]: step function; penalize if repetaed action

--- src/uiadaptationenv.py
+++ src/uiadaptationenv.py
@@ -7,6 +7,8 @@ from environment import Environment
 from uidesign import UIDesign
 import utils
 
+import copy
+
 
 class UIAdaptationEnv (gym.Env):
     '''
@@ -24,7 +26,6 @@ class UIAdaptationEnv (gym.Env):
 
         # We have 4 actions, corresponding to "NoOperate", "DefaultLayout", "left", "down", "right"
         self.action_space = spaces.Discrete(8)
-
 
         self.observation_space = gym.spaces.Dict({
             'layout': gym.spaces.Discrete(2),
@@ -47,44 +48,82 @@ class UIAdaptationEnv (gym.Env):
             })
         })
 
-        
-
-
-
-        # assert render_mode is None or render_mode in self.metadata["render_modes"]
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
     def render(self):
+        '''
+        Returns: None
+        Show the current environment state e.g., the graphical window
+        This method must be implemented, but it's ok to have an empty implementation if
+        rendering is not important
+        '''
+        self.uidesign.render()
+    
+    def close (self):
+        '''
+        Return: None
+        This method is optional. Used to cleanup all resources (threads, GUI, etc)
+        '''
         pass
-        # return super().render()
 
     def step(self, action):
+        initial_design = copy.deepcopy(self.uidesign)
+        print(initial_design)
+        print(self.uidesign)
+        penalize_flag = False
         if action == 0:
             # noop
             pass
         elif action == 1:
             # Change to Grid
-            self.uidesign.change_layout('grid')
+            if self.uidesign.layout == 'grid':
+                penalize_flag = True
+            else:
+                self.uidesign.change_layout('grid')
         elif action == 2:
             # Change to List
-            self.uidesign.change_layout('list')
+            if self.uidesign.layout == 'list':
+                penalize_flag = True
+            else:
+                self.uidesign.change_layout('list')
         elif action == 3:
             # Change to Light color
-            self.uidesign.change_color_scheme('light')
+            if self.uidesign.color_scheme == 'light':
+                penalize_flag = True
+            else:
+                self.uidesign.change_color_scheme('light')
         elif action == 4:
             # Change to Dark color
-            self.uidesign.change_color_scheme('dark')
+            if self.uidesign.color_scheme == 'dark':
+                penalize_flag = True
+            else:
+                self.uidesign.change_color_scheme('dark')
         elif action == 5:
             # Change to default font size.
-            self.uidesign.change_font_size('default')
+            if self.uidesign.font_size == 'default':
+                penalize_flag = True
+            else:
+                self.uidesign.change_font_size('default')
         elif action == 6:
             # Change to small font size
-            self.uidesign.change_font_size('small')
+            if self.uidesign.font_size == 'small':
+                penalize_flag = True
+            else:
+                self.uidesign.change_font_size('small')
         elif action == 7:
             # Change to big font size
-            self.uidesign.change_font_size('big')
-        
+            if self.uidesign.font_size == 'big':
+                penalize_flag = True
+            else:        
+                self.uidesign.change_font_size('big')
 
+        self.user.update_emotion(initial_design, self.uidesign)
+        if penalize_flag:
+            reward = -500
+        else:
+            reward = self.user.get_satisfaction(self.uidesign)
+        print("REWARD: {}".format(reward))
         #return super().step(action)
 
    
